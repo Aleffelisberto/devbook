55318e68613b0336f2f640c15482f4105340f6ad
Alefsandler <aleffelisberto@gmail.com>
2024-04-02 09:34:25 -0300

    feat: implement CreateUser controller

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -1,9 +1,44 @@
 package controllers
 
-import "net/http"
+import (
+	"curso/devbook/src/db"
+	"curso/devbook/src/models"
+	"curso/devbook/src/repositories"
+	"curso/devbook/src/responses"
+	"encoding/json"
+	"io"
+	"net/http"
+)
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Usuário criado"))
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		responses.Error(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var user models.User
+	if err = json.Unmarshal(body, &user); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := db.Connect()
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repository := repositories.NewUserRepository(db)
+	ID, err := repository.Create(user)
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	user.ID = ID
+	responses.JSON(w, http.StatusCreated, user)
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
