9f73457ba61fea027fd22886df37400027bfbc97
Alefsandler <aleffelisberto@gmail.com>
2024-04-02 08:24:06 -0300

    config: add db config

---
+++ src/db/db.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"curso/devbook/src/config"
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Connect() (*sql.DB, error) {
+	db, err := sql.Open("mysql", config.DBURI)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
