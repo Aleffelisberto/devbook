60a8cc60d4f0af3e5bd17821982abd61b90284b5
Alefsandler <aleffelisberto@gmail.com>
2024-04-15 07:42:26 -0300

    feat: add prepare function to users struct

--- src/models/user.go
+++ src/models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type User struct {
 	ID        uint64    `json:"id,omitempty"`
@@ -9,4 +13,40 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"createdAt,omitempty"`
+}
+
+func (u *User) Prepare() error {
+	if err := u.validate(); err != nil {
+		return err
+	}
+
+	u.format()
+
+	return nil
+}
+
+func (u *User) validate() error {
+	if u.Name == "" {
+		return errors.New("o nome é obrigatório e não pode estar em branco")
+	}
+
+	if u.Nick == "" {
+		return errors.New("o nick é obrigatório e não pode estar em branco")
+	}
+
+	if u.Email == "" {
+		return errors.New("o email é obrigatório e não pode estar em branco")
+	}
+
+	if u.Password == "" {
+		return errors.New("a senha é obrigatório e não pode estar em branco")
+	}
+
+	return nil
+}
+
+func (u *User) format() {
+	u.Name = strings.TrimSpace(u.Name)
+	u.Nick = strings.TrimSpace(u.Nick)
+	u.Email = strings.TrimSpace(u.Email)
 }
