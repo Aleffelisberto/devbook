14956465900e2a436782f356366898640e094b45
Alefsandler <aleffelisberto@gmail.com>
2024-04-15 07:42:50 -0300

    feat: add FindAllByNameOrNick method

--- src/repositories/users.go
+++ src/repositories/users.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"curso/devbook/src/models"
 	"database/sql"
+	"fmt"
 )
 
 type users struct {
@@ -31,4 +32,35 @@ func (repo users) Create(user models.User) (uint64, error) {
 	}
 
 	return uint64(lastInsertID), err
+}
+
+func (repo users) FindAllByNameOrNick(nameOrNick string) ([]models.User, error) {
+	nameOrNickFilter := fmt.Sprintf("%%%s%%", nameOrNick)
+
+	rows, err := repo.db.Query("SELECT id, name, nick, email, createdAt FROM users WHERE name LIKE ? OR nick LIKE ?", nameOrNickFilter, nameOrNickFilter)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var users []models.User
+	for rows.Next() {
+		var user models.User
+
+		if err = rows.Scan(
+			&user.ID,
+			&user.Name,
+			&user.Nick,
+			&user.Email,
+			&user.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
 }
